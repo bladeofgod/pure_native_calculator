d84401129d2e865eee9bd3e12be481468d77a557
bladeofgod <450103187@qq.com>
2021-06-19 21:58:25 +0800

    add egl,callback framework code

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -37,6 +37,7 @@ struct engine{
     const ASensor       *accelerometerSenor;
     ASensorEventQueue   *sensorEventQueue;
 
+    //will draw when value = 1
     int animation;
     EGLDisplay display;
     EGLSurface surface;
@@ -109,13 +110,32 @@ void android_main(struct android_app* state) {
                 //is user event data
                 if(engine.accelerometerSenor != nullptr) {
                     ASensorEvent   event;
-                    while (ASensorEventQueue_getEvents(engine.sensorEventQueue,
-                                                       &event,1) > 0) {
-
+                    while (ASensorEventQueue_getEvents(engine.sensorEventQueue,//add queue
+                                                       &event,//event data
+                                                       1)
+                                                       > 0) {
+                        LOGI("accelerometer: x=%f   y=%f   z=%f",
+                             event.acceleration.x,
+                             event.acceleration.y,
+                             event.acceleration.z);
                     }
                 }
             }
 
+            //if app is dead, this {destroyRequested} will non-zero
+            if(state->destroyRequested != 0) {
+                engine_term_display(&engine);
+                return;
+            }
+
+        }
+
+        if(engine.animation) {
+            engine.state.angle += .01f;
+            if(engine.state.angle > 1 ) {
+                engine.state.angle = 0;
+            }
+            engine_draw_frame(&engine);
         }
 
 
@@ -124,11 +144,163 @@ void android_main(struct android_app* state) {
 
 }
 
+static int engine_init_display(struct engine* engine) {
+
+
+    //rgb 888
+
+    const EGLint attributes[] = {
+            EGL_SURFACE_TYPE,
+            EGL_WINDOW_BIT,
+            EGL_BLUE_SIZE,8,
+            EGL_GREEN_SIZE,8,
+            EGL_RED_SIZE,8,
+            EGL_NONE
+    };
+
+    EGLint w,h,format;
+    EGLint numConfigs;
+    EGLConfig config = nullptr;
+    EGLSurface surface;
+    EGLContext context;
+
+    EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+
+    //init egl
+    // display, version..
+    eglInitialize(display, nullptr, nullptr);
+
+    eglChooseConfig(display,attributes, nullptr,0,&numConfigs);
+
+    std::unique_ptr<EGLConfig[]> supportedConfigs(new EGLConfig[numConfigs]);
+
+    assert(supportedConfigs);
+    /*
+     * {attributes} is your prefer
+     * {supportedConfigs.get()} is system result
+     */
+    eglChooseConfig(display,attributes,supportedConfigs.get(),numConfigs,&numConfigs);
+    assert(numConfigs);
+
+    auto i = 0;
+    // filter a config like rgb 888
+    for(;i < numConfigs;i++){
+        auto &cfg = supportedConfigs[i];
+        EGLint r,g,b,d;
+        if(eglGetConfigAttrib(display,cfg,EGL_RED_SIZE,&r)
+           && eglGetConfigAttrib(display,cfg,EGL_GREEN_SIZE,&g)
+           && eglGetConfigAttrib(display,cfg,EGL_BLUE_SIZE,&b)
+           && eglGetConfigAttrib(display,cfg,EGL_DEPTH_SIZE,&d)
+           && r ==8 && g == 8 && b == 8 && d == 8) {
+            config = supportedConfigs[i];
+            break;
+        }
+    }
+
+    //if not match, chose first config
+    if(i == numConfigs) {
+        config = supportedConfigs[0];
+    }
+
+    //dont have any configs
+    if(config == nullptr) {
+        LOGE("unable to initialize EGLConfig");
+        return -1;
+    }
+
+    eglGetConfigAttrib(display,config,EGL_NATIVE_VISUAL_ID,&format);
+
+    //create egl surface from activity's window
+    surface = eglCreateWindowSurface(display,config,engine->app->window, nullptr);
+    context = eglCreateContext(display,config, nullptr, nullptr);
+
+    //switch context,make sure. it will bind display&surface to context
+    // than opengl's functions will work.
+    //read/write on a same surface
+    if(eglMakeCurrent(display,surface,surface,context) == EGL_FALSE) {
+        LOGE("unable to egl make current");
+        return -1;
+    }
+
+    eglQuerySurface(display,surface,EGL_WIDTH,&w);
+    eglQuerySurface(display,surface,EGL_HEIGHT,&h);
+
+    LOGI("surface width: %i , height:  %i",w,h);
+
+    //save egl
+    engine->display = display;
+    engine->context = context;
+    engine->surface = surface;
+    engine->width = w;
+    engine->height = h;
+    engine->state.angle = 0;
+
+    auto opengl_info = {GL_VENDOR,GL_RENDERER,GL_VERSION,GL_EXTENSIONS};
+
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT,GL_FASTEST);
+    glEnable(GL_CULL_FACE);
+    glShadeModel(GL_SMOOTH);
+    glDisable(GL_DEPTH_TEST);
+
+    return 0;
+
+}
+
+
+/*
+ * draw
+ */
+
+static void engine_draw_frame(struct engine * engine) {
+    if(engine->display == nullptr) {
+        return;
+    }
+
+    //set pre-fill-color
+    glClearColor(((float )engine->state.x)/engine->width,
+                 engine->state.angle,
+                 ((float )engine->state.y)/engine->height,
+                 1);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    eglSwapBuffers(engine->display,engine->surface);
+
+}
+
+
+static void engine_term_display(struct engine* engine) {
+    if(engine->display != EGL_NO_DISPLAY) {
+        eglMakeCurrent(engine->display,EGL_NO_SURFACE,EGL_NO_SURFACE,EGL_NO_CONTEXT);
+        if(engine->context != EGL_NO_CONTEXT) {
+            eglDestroyContext(engine->display,engine->context);
+        }
+        if(engine->surface != EGL_NO_SURFACE) {
+            eglDestroySurface(engine->display,engine->surface);
+        }
+    }
+
+    engine->animation = 0;
+    engine->display = EGL_NO_DISPLAY;
+    engine->surface = EGL_NO_SURFACE;
+    engine->context = EGL_NO_CONTEXT;
+
+}
+
 /*
  * android input events call back
  */
 
-static int32_t engine_handle_input(struct android_app* app, AInputEvent* event) {}
+static int32_t engine_handle_input(struct android_app* app, AInputEvent* event) {
+    auto *engine = (struct engine*)app->userData;
+    if(AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION) {
+        engine->animation = 1;
+        engine->state.x = AMotionEvent_getX(event,0);
+        engine->state.y = AMotionEvent_getY(event,0);
+        return 1;
+    }
+    return 0;
+}
 
 
 /*
@@ -139,6 +311,52 @@ static int32_t engine_handle_input(struct android_app* app, AInputEvent* event) 
 static void engine_handle_cmd(struct android_app* app,int32_t cmd) {
 
     auto* engine = (struct engine*)app->userData;
+
+    //some call backs ...
+    switch (cmd) {
+        case APP_CMD_SAVE_STATE:
+            //alloc memory for save app's state
+            engine->app->savedState = malloc(sizeof(struct saved_state));
+
+            //do save
+            *((struct saved_state*)engine->app->savedState) = engine->state;
+            engine->app->savedStateSize = sizeof(struct saved_state);
+            break;
+        case APP_CMD_INIT_WINDOW:
+            //window has initiate
+            if(engine->app->window != nullptr) {
+                //init egl display
+                engine_init_display(engine);
+                engine_draw_frame(engine);
+
+            }
+            break;
+        case APP_CMD_TERM_WINDOW:
+            engine_term_display(engine);
+            break;
+        case APP_CMD_GAINED_FOCUS:
+            if(engine->accelerometerSenor != nullptr) {
+                //receive app's event
+                ASensorEventQueue_enableSensor(engine->sensorEventQueue,
+                                               engine->accelerometerSenor);
+
+                ASensorEventQueue_setEventRate(engine->sensorEventQueue,
+                                               engine->accelerometerSenor,
+                                               (1000L/60) * 1000);
+            }
+            break;
+        case APP_CMD_LOST_FOCUS:
+            if(engine->accelerometerSenor != nullptr) {
+                ASensorEventQueue_disableSensor(engine->sensorEventQueue,
+                                                engine->accelerometerSenor);
+            }
+            engine->animation = 0;
+            engine_draw_frame(engine);
+            break;
+
+
+    }
+
 }
 
 /*
--- app/src/main/cpp/native_activity_method.h
+++ app/src/main/cpp/native_activity_method.h
@@ -11,7 +11,18 @@ static void engine_handle_cmd(struct android_app* app,int32_t cmd);
 //input event's call back
 static int32_t engine_handle_input(struct android_app* app,AInputEvent* event);
 
+//init display,egl
+static int engine_init_display(struct engine* engine);
+
+//terminate display
+static void engine_term_display(struct engine* engine);
+
+//draw
+static void engine_draw_frame(struct engine * engine);
+
 //fetch sensor manager
 ASensorManager* acquireASensorManagerInstance(android_app* app);
+
+
 
 #endif //PURENATIVECALCULATORDEMO_NATIVE_ACTIVITY_METHOD_H
