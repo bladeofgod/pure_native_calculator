af766e626ec4c7d367bce8be74c99f05e36bf814
bladeofgod <450103187@qq.com>
2021-07-04 17:42:14 +0800

    working on draw rect view...

--- app/src/main/cpp/gl_test.h
+++ app/src/main/cpp/gl_test.h
@@ -60,36 +60,36 @@ static void gl_draw_some_rect(struct engine *engine) {
 //}
 
 
-static void gl_draw_rect(struct engine *engine) {
-
-    GLfloat vVertices[] = {
-            -0.5f, 0.5f, 0.0f,
-            0.5f, 0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f
-    };
-
-    // 1. 通知OpenGL ES用于绘制的2D渲染表面的原点(x,y)坐标，宽度和高度
-    glViewport(0,0,engine->width,engine->height);
-    // 2. 清除颜色缓冲区；有颜色、深度和模板缓冲区
-    glClear(GL_COLOR_BUFFER_BIT);
-    // 3. 将程序设置为活程序
-    glUseProgram(engine->programObject);
-    // void glVertexAttribPointer (GLuint index, GLint size, GLenum type, GLboolean normalized,
-    // GLsizei stride, const void *pointer);
-    // index为第几个属性，属性有顶点的位置为0/纹理为1/法线为3；size为一个顶点所有数据的个数，这里XYZ为3个；
-    // type为顶点描述数据的类型，这里为FLOAT；normalized为是否需要显卡把数据归一化到-1到+1区间，这里不需要为FALSE
-    // stride连续顶点属性之间的偏移量，0表示他们为紧密排列在一起的。pointer为顶点数组
-    // 4. 加载顶点位置到GL中
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,vVertices);
-    // 5. 使用顶点数组顶点位置属性，也就是0属性；如果什么都不使用的话，就使用常量顶点属性0
-    glEnableVertexAttribArray(0);
-    // 6. 设置线粗
-    glLineWidth(10);
-    // 7. 绘制矩形
-    glDrawArrays(GL_LINE_LOOP, 0, 4);
-
-    eglSwapBuffers(engine->display,engine->surface);
-}
+//static void gl_draw_rect(struct engine *engine) {
+//
+//    GLfloat vVertices[] = {
+//            -0.5f, 0.5f, 0.0f,
+//            0.5f, 0.5f, 0.0f,
+//            0.5f, -0.5f, 0.0f,
+//            -0.5f, -0.5f, 0.0f
+//    };
+//
+//    // 1. 通知OpenGL ES用于绘制的2D渲染表面的原点(x,y)坐标，宽度和高度
+//    glViewport(0,0,engine->width,engine->height);
+//    // 2. 清除颜色缓冲区；有颜色、深度和模板缓冲区
+//    glClear(GL_COLOR_BUFFER_BIT);
+//    // 3. 将程序设置为活程序
+//    glUseProgram(engine->programObject);
+//    // void glVertexAttribPointer (GLuint index, GLint size, GLenum type, GLboolean normalized,
+//    // GLsizei stride, const void *pointer);
+//    // index为第几个属性，属性有顶点的位置为0/纹理为1/法线为3；size为一个顶点所有数据的个数，这里XYZ为3个；
+//    // type为顶点描述数据的类型，这里为FLOAT；normalized为是否需要显卡把数据归一化到-1到+1区间，这里不需要为FALSE
+//    // stride连续顶点属性之间的偏移量，0表示他们为紧密排列在一起的。pointer为顶点数组
+//    // 4. 加载顶点位置到GL中
+//    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,vVertices);
+//    // 5. 使用顶点数组顶点位置属性，也就是0属性；如果什么都不使用的话，就使用常量顶点属性0
+//    glEnableVertexAttribArray(0);
+//    // 6. 设置线粗
+//    glLineWidth(10);
+//    // 7. 绘制矩形
+//    glDrawArrays(GL_LINE_LOOP, 0, 4);
+//
+//    eglSwapBuffers(engine->display,engine->surface);
+//}
 
 #endif //PURENATIVECALCULATORDEMO_GL_TEST_H
